bb73c62569c11fcc12264085d49fdbd12746a753
Tima-simati <timaaks0ul@gmail.com>
2026-07-23 14:44:34 +0200

    created project and added first variables

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*Design a game where the user can play a make-believe slot machine. The user will be asked to make a wager to play various lines in a 3 x 3 grid. 
+             * They can play center line, all three horizontal lines, all vertical lines and diagonals.
+            For instance the user can enter $3 dollars and play all three horizontal lines. If the top line hits a winning combination, they earn $1 dollar for that line.
+
+            rocket Tips: The mention of a grid here should be a dead giveaway that you need a 2D array. 
+            You will also need functionality that can check a horizontal line, a vertical line and a diagonal. 
+            Depending on the number of lines they play, you may need to execute all three of these statements one or multiple times to look for winning lines. 
+            If they are playing three lines, you would call your horizontal line check function three times... one for the top row, one for the center row and one for the bottom row. 
+            Each of these row checking algorithms will then need to look for winning combinations. The result is then dumped into the player’s money total. 
+            As for the mechanism to determine what the wheels produce per spin, use a random number generating function.
+            
+            -first think about how you want your slotmachine to work
+            fill array with random numbers
+            output that array
+            check if middle row is all the same
+            output win /lose
+            keep track of money
+            implement more game modes (multiple lines / diagonals / se above description)
+             */
+            int playerWallet = 30;
+            int[,] SlotMachineArray = new int[3,3];
+            const int EARNING_FOR_ONE_LINE = 1;
+
         }
     }
 }
