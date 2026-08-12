265e4d2f7ef27ca1a6ed667ac830fe99830200f6
Tima-simati <timaaks0ul@gmail.com>
2026-08-12 23:48:15 +0200

    starting refactoring slot machine; added enums in Logic.cs and added text screens to UI.cs

---
+++ SlotMachine/Logic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlotMachine
+{
+    public static class Logic
+    {
+        public const int GRID_SIZE = 3;
+        public const int CENTRAL_LINE_INDEX_OF_GRID = GRID_SIZE / 2;
+        public const int LAST_INDEX_GRID = GRID_SIZE - 1;
+        enum gameMode
+        {
+            ALL_HORIZONTAL,
+            ALL_VERTICAL,
+            ALL_DIAGONAL,
+            ALL_LINES
+        }
+        enum betSize
+        {
+            BET_ONE,
+            BET_THREE
+        }
+        enum gameState
+        {
+            CONTINUE_GAME,
+            END_GAME
+        }
+
+    }
+}
---
+++ SlotMachine/UI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlotMachine
+{
+    public static class UI
+    {
+      public static void WelcomeScreen(int credits)
+        {
+            Console.WriteLine($"SLOTMACHINE ARCADE!!!\nYou start with a balance of {credits}$.\nCome and Play!");
+            Console.WriteLine("One Game costs 1$. One WIN is considered, that only one matching row counts. \nYou win 1$ and your investment.");
+            Console.WriteLine("You also have an option to bet 3$ and play 3 other game modes. \nHigher Risk, Higher Reward!");
+        }
+        public static void ShowCurrentBalance(int balance)
+        {
+            Console.WriteLine($"Your current balance: {balance}$");
+        }
+        public static void ShowZeroBalance()
+        {
+            Console.WriteLine("You are out of credits.");
+        }
+        public static void ShowEndScreen(int balance)
+        {
+            Console.WriteLine($"Game ended! Your balance is: {balance}");
+        }
+    }
+}
