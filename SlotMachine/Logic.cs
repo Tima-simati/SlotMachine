be4d018c2ebfa14a8f282cc35c710251aab9bac8
Tima-simati <timaaks0ul@gmail.com>
2026-08-14 01:10:18 +0200

    implemented gameMode switch cases in Program.cs; still need to implement winning BET_THREE wager back

--- SlotMachine/Logic.cs
+++ SlotMachine/Logic.cs
@@ -44,17 +44,89 @@ namespace SlotMachine
             }
             return false;
         }
-        public static void PlayAllHorizontalLines(int[,] array, int balance)
+        public static int PlayAllHorizontalLines(int[,] array)
         {
-          
+            int win = 0;
+            bool allEqual = true;
+            for (int i = 0; i < Constants.GRID_SIZE; i++)
+            {
+                for (int j = 0; j < Constants.LAST_INDEX_GRID; j++)
+                {
+                    if (array[i, j] != array[i, j + 1])
+                    {
+                        allEqual = false;
+                        Console.WriteLine($"No matching line for row {i}!");
+                        break;
+                    }
+                }
+                if (allEqual)
+                {
+                    Console.WriteLine($"You Won! Row {i} line was a match.");
+                    win += Constants.EARNING_FOR_ONE_LINE;                    
+                }
+                allEqual = true;
+            }
+            return win;
         }
-        public static void PlayAllVerticalLines(int[,] array, int balance)
+        public static int PlayAllVerticalLines(int[,] array)
         {
-
+            int win = 0;
+            bool allEqual = true;
+            for (int i = 0; i < Constants.GRID_SIZE; i++)
+            {
+                for (int j = 0; j < Constants.LAST_INDEX_GRID; j++)
+                {
+                    if (array[j, i] != array[j + 1, i])
+                    {
+                        allEqual = false;
+                        Console.WriteLine($"No matching line for column {i}!");
+                        break;
+                    }
+                }
+                if (allEqual)
+                {
+                    Console.WriteLine("You Won! A vertical line was a match.");
+                    win += Constants.EARNING_FOR_ONE_LINE;                    
+                }
+                allEqual = true;
+            }
+            return win;
         }
-        public static void PlayAllDiagonalLines(int[,] array, int balance)
+        public static int PlayAllDiagonalLines(int[,] array)
         {
-
+            int win = 0;
+            bool allEqual = true;
+            for (int i = 0, j = 0; i < Constants.LAST_INDEX_GRID; i++, j++)
+            {
+                if (array[i, j] != array[i + 1, j + 1])
+                {
+                    allEqual = false;
+                    Console.WriteLine($"No matching declining diagonal line!");
+                    break;
+                }
+            }
+            if (allEqual)
+            {
+                Console.WriteLine("You Won! Declining diagnoal line was a match.");
+                win += Constants.EARNING_FOR_ONE_LINE;
+               
+            }
+            allEqual = true;
+            for (int i = Constants.LAST_INDEX_GRID, j = 0; i > 0; i--, j++)
+            {
+                if (array[i, j] != array[i - 1, j + 1])
+                {
+                    allEqual = false;
+                    Console.WriteLine($"No matching inclining diagonal line!");
+                    break;
+                }
+            }
+            if (allEqual)
+            {
+                Console.WriteLine("You Won! Inclining diagonal line was a match.");
+                win += Constants.EARNING_FOR_ONE_LINE;
+            }
+            return win;
         }
     }
 }
--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -9,12 +9,7 @@ namespace SlotMachine
     {
         static void Main(string[] args)
         {
-            const int STARTING_CREDITS = 10;
-            const int END_GAME = 0;
-            //declaring boundaries of numbers displayed in slotMachine
 
-            const int BET_ONE = 1;  //for only center horizontal game
-            const int BET_THREE = 3;//for all other game modes
             const int EARNING_FOR_ONE_LINE = 1;
             const int CHOOSE_ALL_HORIZONTAL = 1;
             const int CHOOSE_ALL_VERTICAL = 2;
@@ -28,7 +23,7 @@ namespace SlotMachine
             int playerWallet = Constants.STARTING_CREDITS;  //money balance of player
             int[,] slotMachineArray = new int[Constants.GRID_SIZE, Constants.GRID_SIZE];//array of slotMachine initialized
             int continueGame = 1;
-            
+
             int gameMode = 0; //var to choose one of the game modes for 3$ wager
             bool allLinesEnabled = false; //if all Lines are considered in a game
             int winCounter = 0; //counter for 3$ bets; if one line wins, you get wager back
@@ -57,102 +52,118 @@ namespace SlotMachine
                 //3$ GAME: check for all horizontal, vertical and diagonal lines matching
                 if (bet == Constants.BET_THREE)
                 {
-                    gameMode = UI.ShowGameModeOptions();                    
-                 
-                    if (gameMode == CHOOSE_ALL_LINES)   //check for all lines
+                    gameMode = UI.ShowGameModeOptions();
+
+                    switch (gameMode)
                     {
-                        gameMode = CHOOSE_ALL_HORIZONTAL; //since all lines to play are chosen; switch to horizontal lines check
-                        allLinesEnabled = true;
+                        case (int)Enums.GameMode.CHOOSE_ALL_HORIZONTAL:
+                            playerWallet += Logic.PlayAllHorizontalLines(slotMachineArray);
+                            break;
+                        case (int)Enums.GameMode.CHOOSE_ALL_VERTICAL:
+                            playerWallet += Logic.PlayAllVerticalLines(slotMachineArray);
+                            break;
+                        case (int)Enums.GameMode.CHOOSE_ALL_DIAGONAL:
+                            playerWallet += Logic.PlayAllDiagonalLines(slotMachineArray);
+                            break;
+                        case (int)Enums.GameMode.CHOOSE_ALL_LINES:
+                            playerWallet += Logic.PlayAllHorizontalLines(slotMachineArray) + Logic.PlayAllVerticalLines(slotMachineArray) + Logic.PlayAllDiagonalLines(slotMachineArray);
+                            break;
                     }
-                    if (gameMode == CHOOSE_ALL_HORIZONTAL) //check for any of the horizontal lines winning
-                    {
-                        bool allEqual = true;
-                        for (int i = 0; i < GRID_SIZE; i++)
-                        {
-                            for (int j = 0; j < LAST_INDEX_GRID; j++)
-                            {
-                                if (slotMachineArray[i, j] != slotMachineArray[i, j + 1])
-                                {
-                                    allEqual = false;
-                                    Console.WriteLine($"No matching line for row {i}!");
-                                    break;
-                                }
-                            }
-                            if (allEqual)
-                            {
-                                Console.WriteLine($"You Won! Row {i} line was a match.");
-                                playerWallet += EARNING_FOR_ONE_LINE;
-                                winCounter++;
-                            }
-                            allEqual = true;
-                        }
-                        if (allLinesEnabled == true)
-                        {
-                            gameMode = CHOOSE_ALL_VERTICAL; //for CHOOSE_ALL_LINES switch from horizontal check to vertical check
-                        }
-                    }
-                    if (gameMode == CHOOSE_ALL_VERTICAL) //check for vertical line matching
-                    {
-                        bool allEqual = true;
-                        for (int i = 0; i < GRID_SIZE; i++)
-                        {
-                            for (int j = 0; j < LAST_INDEX_GRID; j++)
-                            {
-                                if (slotMachineArray[j, i] != slotMachineArray[j + 1, i])
-                                {
-                                    allEqual = false;
-                                    Console.WriteLine($"No matching line for column {i}!");
-                                    break;
-                                }
-                            }
-                            if (allEqual)
-                            {
-                                Console.WriteLine("You Won! A vertical line was a match.");
-                                playerWallet += EARNING_FOR_ONE_LINE;
-                                winCounter++;
-                            }
-                            allEqual = true;
-                        }
-                        if (allLinesEnabled == true)
-                        {
-                            gameMode = CHOOSE_ALL_DIAGONAL; //for CHOOSE_ALL_LINES switch from vertical check to horizontal check
-                        }
-                    }
-                    if (gameMode == CHOOSE_ALL_DIAGONAL) //check for diagnoal line matching
-                    {
-                        bool allEqual = true;
-                        for (int i = 0, j = 0; i < LAST_INDEX_GRID; i++, j++)
-                        {
-                            if (slotMachineArray[i, j] != slotMachineArray[i + 1, j + 1])
-                            {
-                                allEqual = false;
-                                Console.WriteLine($"No matching declining diagonal line!");
-                                break;
-                            }
-                        }
-                        if (allEqual)
-                        {
-                            Console.WriteLine("You Won! Declining diagnoal line was a match.");
-                            playerWallet += EARNING_FOR_ONE_LINE;
-                            winCounter++;
-                        }
-                        allEqual = true;
-                        for (int i = LAST_INDEX_GRID, j = 0; i > 0; i--, j++)
-                        {
-                            if (slotMachineArray[i, j] != slotMachineArray[i - 1, j + 1])
-                            {
-                                allEqual = false;
-                                Console.WriteLine($"No matching inclining diagonal line!");
-                                break;
-                            }
-                        }
-                        if (allEqual)
-                        {
-                            Console.WriteLine("You Won! Inclining diagonal line was a match.");
-                            playerWallet += EARNING_FOR_ONE_LINE;
-                            winCounter++;
-                        }
-                    }
+
+                    //if (gameMode == CHOOSE_ALL_LINES)   //check for all lines
+                    //{
+                    //    gameMode = CHOOSE_ALL_HORIZONTAL; //since all lines to play are chosen; switch to horizontal lines check
+                    //    allLinesEnabled = true;
+                    //}
+                    //if (gameMode == CHOOSE_ALL_HORIZONTAL) //check for any of the horizontal lines winning
+                    //{
+                    //    bool allEqual = true;
+                    //    for (int i = 0; i < GRID_SIZE; i++)
+                    //    {
+                    //        for (int j = 0; j < LAST_INDEX_GRID; j++)
+                    //        {
+                    //            if (slotMachineArray[i, j] != slotMachineArray[i, j + 1])
+                    //            {
+                    //                allEqual = false;
+                    //                Console.WriteLine($"No matching line for row {i}!");
+                    //                break;
+                    //            }
+                    //        }
+                    //        if (allEqual)
+                    //        {
+                    //            Console.WriteLine($"You Won! Row {i} line was a match.");
+                    //            playerWallet += EARNING_FOR_ONE_LINE;
+                    //            winCounter++;
+                    //        }
+                    //        allEqual = true;
+                    //    }
+                    //    if (allLinesEnabled == true)
+                    //    {
+                    //        gameMode = CHOOSE_ALL_VERTICAL; //for CHOOSE_ALL_LINES switch from horizontal check to vertical check
+                    //    }
+                    //}
+                    //if (gameMode == CHOOSE_ALL_VERTICAL) //check for vertical line matching
+                    //{
+                    //    bool allEqual = true;
+                    //    for (int i = 0; i < GRID_SIZE; i++)
+                    //    {
+                    //        for (int j = 0; j < LAST_INDEX_GRID; j++)
+                    //        {
+                    //            if (slotMachineArray[j, i] != slotMachineArray[j + 1, i])
+                    //            {
+                    //                allEqual = false;
+                    //                Console.WriteLine($"No matching line for column {i}!");
+                    //                break;
+                    //            }
+                    //        }
+                    //        if (allEqual)
+                    //        {
+                    //            Console.WriteLine("You Won! A vertical line was a match.");
+                    //            playerWallet += EARNING_FOR_ONE_LINE;
+                    //            winCounter++;
+                    //        }
+                    //        allEqual = true;
+                    //    }
+                    //    if (allLinesEnabled == true)
+                    //    {
+                    //        gameMode = CHOOSE_ALL_DIAGONAL; //for CHOOSE_ALL_LINES switch from vertical check to horizontal check
+                    //    }
+                    //}
+                    //if (gameMode == CHOOSE_ALL_DIAGONAL) //check for diagnoal line matching
+                    //{
+                    //    bool allEqual = true;
+                    //    for (int i = 0, j = 0; i < LAST_INDEX_GRID; i++, j++)
+                    //    {
+                    //        if (slotMachineArray[i, j] != slotMachineArray[i + 1, j + 1])
+                    //        {
+                    //            allEqual = false;
+                    //            Console.WriteLine($"No matching declining diagonal line!");
+                    //            break;
+                    //        }
+                    //    }
+                    //    if (allEqual)
+                    //    {
+                    //        Console.WriteLine("You Won! Declining diagnoal line was a match.");
+                    //        playerWallet += EARNING_FOR_ONE_LINE;
+                    //        winCounter++;
+                    //    }
+                    //    allEqual = true;
+                    //    for (int i = LAST_INDEX_GRID, j = 0; i > 0; i--, j++)
+                    //    {
+                    //        if (slotMachineArray[i, j] != slotMachineArray[i - 1, j + 1])
+                    //        {
+                    //            allEqual = false;
+                    //            Console.WriteLine($"No matching inclining diagonal line!");
+                    //            break;
+                    //        }
+                    //    }
+                    //    if (allEqual)
+                    //    {
+                    //        Console.WriteLine("You Won! Inclining diagonal line was a match.");
+                    //        playerWallet += EARNING_FOR_ONE_LINE;
+                    //        winCounter++;
+                    //    }
+                    //}
                 }
                 //giving back wager, if winCounter for 3$ game was more than 0
                 if (winCounter > 0)
@@ -173,16 +184,16 @@ namespace SlotMachine
                 //ask user to continue game
                 Console.WriteLine("Do you want to continue?\nType 1 for continue,\ntype 0 to end the game.");
                 int.TryParse(Console.ReadLine(), out continueGame);
-                while (continueGame != 1 || continueGame != END_GAME)
+                while (continueGame != 1 || continueGame != Constants.END_GAME)
                 {
-                    if (continueGame == 1 || continueGame == END_GAME)
+                    if (continueGame == 1 || continueGame == Constants.END_GAME)
                     {
                         break;
                     }
                     Console.WriteLine($"No option found for {continueGame} ! Only press 0 or 1");
                     int.TryParse(Console.ReadLine(), out continueGame);
                 }
-                if (continueGame == END_GAME)
+                if (continueGame == Constants.END_GAME)
                 {
                     break;
                 }
